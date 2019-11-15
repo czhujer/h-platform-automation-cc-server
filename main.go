00e3958b7860bb1fb6f8dcd1d2fe84768f6a35ee
Patrik Majer <patrik.majer.pisek@gmail.com>
2019-11-15 18:50:47 +0100

    add parsing webhook data

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
 	"io/ioutil"
@@ -10,6 +11,32 @@ import (
 )
 
 var receivedData []byte
+
+type CalculoidData struct {
+	CalculatorId        string               `json:"calculatorId"`
+	Email               string               `json:"email"`
+	Id                  string               `json:"id"`
+	CalculoidDataParams CalculoidDataParams  `json:"params"`
+	UserSignature       string               `json:"userSignature"`
+	FromEmail           string               `json:"fromEmail"`
+	Fields              CalculoidDataField   `json:"fields"`
+	Payment             CalculoidDataPayment `json:"Payment"`
+}
+
+type CalculoidDataParams struct {
+}
+
+type CalculoidDataField map[string]CalculoidDataFields
+
+type CalculoidDataFields struct {
+	CalculatorFieldId string `json:"calculatorFieldId"`
+	Name              string `json:"name"`
+}
+
+type CalculoidDataPayment struct {
+	Id     string `json:"id"`
+	Status string `json:"status"`
+}
 
 func homeLink(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "")
@@ -51,7 +78,33 @@ func queryParams(w http.ResponseWriter, r *http.Request) {
 }
 
 func calculoidWebhookParser() {
-	log.Printf("received data for parsing: %s \n", receivedData)
+	var parsedData CalculoidData
+
+	//log.Printf("received data for parsing: %s \n", receivedData)
+
+	err := json.Unmarshal([]byte(receivedData), &parsedData)
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+
+	log.Println("received data after parsing:")
+
+	fmt.Printf("\ncalculatorId: %v\n", parsedData.CalculatorId)
+	fmt.Printf("email: %s\n", parsedData.Email)
+
+	fmt.Printf("fromEmail: %s\n", parsedData.FromEmail)
+
+	for key := range parsedData.Fields {
+		fmt.Printf("Fields: Key: %s Values: ", key)
+		fmt.Printf("calculatorFieldId: %s ", parsedData.Fields[key].CalculatorFieldId)
+		fmt.Printf("calculatorFieldId: %s ", parsedData.Fields[key].Name)
+
+		fmt.Println("")
+	}
+
+	fmt.Printf("Payment ID: %v and Status: %v \n", parsedData.Payment.Id, parsedData.Payment.Status)
+
+	fmt.Println("")
 }
 
 func main() {
