1dd2fdcc817c3e12e78199290c8cf57e63b47c2f
Patrik Majer <patrik.majer.pisek@gmail.com>
2020-09-05 20:10:09 +0200

    remove owncloudstack create logic

--- terraform/owncloudstack/main.go
+++ terraform/owncloudstack/main.go
@@ -1,50 +1,17 @@
 package owncloudstack
 
-import (
-	"context"
-	"fmt"
-	"github.com/hashicorp/terraform-exec/tfexec"
-	"log"
-)
-
-// TODO
-// generate oc-xyz.tfvars
-//
-// example:
-// vmname = "oc-xyz"
-// vmip = "10.1.2.4"
-// vm_data_disk_size = "10"
-
 func Create() error {
 
-	var terraformPath = "/opt/terraform_0.13.2/terraform"
-	var terraformWorkingDir = "/root/h-platform-automation-core/tf-owncloud"
+	//var terraformPath = "/opt/terraform_0.13.2/terraform"
+	//var terraformWorkingDir = "/root/h-platform-automation-core/tf-owncloud"
 
-	tf, err := tfexec.NewTerraform(terraformWorkingDir, terraformPath)
-	if err != nil {
-		log.Printf("terraform: setup error: %s", err)
-		return err
-	}
+	//TODO
+	// generate oc-3xy.tfvars
+	// wrapper script: tf-owncloud-generate-tfvars.sh
 
-	err = tf.Init(context.Background(), tfexec.Upgrade(true), tfexec.LockTimeout("60s"))
-	if err != nil {
-		log.Printf("terraform: init error: %s", err)
-		return err
-	}
-
-	// TODO
-	// add PlanOptions for oc-xyz.tfvars
-
-	rs, err := tf.Plan(context.Background())
-	if err != nil {
-		log.Printf("terraform: plan error: %s", err)
-		return err
-	}
-
-	// TODO
-	// add terraform apply
-
-	fmt.Println(rs)
+	//TODO
+	// run terraform
+	// wrapper script: tf-owncloud-run-terraform.sh
 
 	return nil
 }
