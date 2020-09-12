7696fa1817b27d990ed62b29a501c6e4e1a7d198
Patrik Majer <patrik.majer.pisek@gmail.com>
2020-09-12 19:16:38 +0200

    add run terraform

--- terraform/owncloudstack/main.go
+++ terraform/owncloudstack/main.go
@@ -7,17 +7,38 @@ import (
 )
 
 func Create() error {
+	//TODO
+	// add tracing support
 	var terraformScriptDir = "/root/h-platform-automation-core/tf-owncloud/scripts"
 
-	cmd := exec.Command(fmt.Sprintf("%s/tf-owncloud-generate-tfvars.sh", terraformScriptDir))
-	if err := cmd.Run(); err != nil {
+	cmdGenerate := exec.Command(fmt.Sprintf("%s/tf-owncloud-generate-tfvars.sh", terraformScriptDir))
+	outputGenerate, err := cmdGenerate.CombinedOutput()
+
+	//TODO
+	// add terraform output to http response
+	if err != nil {
 		log.Printf("terraform: generate tfvars failed: %s", err)
+	}
+	log.Printf("terraform: generate tfvars output:")
+	log.Printf("%s", outputGenerate)
+
+	if err != nil {
 		return err
 	}
 
+	cmdRun := exec.Command(fmt.Sprintf("%s/tf-owncloud-run-terraform.sh", terraformScriptDir))
+	outputRun, err := cmdRun.CombinedOutput()
+
 	//TODO
-	// run terraform
-	// wrapper script: tf-owncloud-run-terraform.sh
+	// add terraform output to http response
+	if err != nil {
+		log.Printf("terraform: run failed: %s", err)
+	}
+	log.Printf("terraform: run output:")
+	log.Printf("%s", outputRun)
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
